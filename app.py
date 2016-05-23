8fdb5bd2839e782a0d9d5c0793fcf39c9b9681c4
Josh Cadruvi <jcadruvi78@yahoo.com>
2016-05-22 18:21:47 -0700

    Adding gevent.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from gevent.pywsgi import WSGIServer
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,4 +11,5 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    ws = WSGIServer(('0.0.0.0', 5001), app)
+    ws.serve_forever()
